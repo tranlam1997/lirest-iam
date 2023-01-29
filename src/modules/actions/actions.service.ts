7aef59b640ce292d961ba0c504663ce89a45ddda
Tran Tuan Lam <foet1997@gmail.com>
2023-01-29 21:20:06 +0700

    Implement get all actions api

--- src/modules/actions/actions.service.ts
+++ src/modules/actions/actions.service.ts
@@ -5,5 +5,10 @@ export const ActionsService = {
   async createAction(action: Action) {
     const newClient = await ActionsRepository.create(action);
     return newClient;
+  },
+
+  async getAllActions() {
+    const actions = await ActionsRepository.findAll();
+    return actions;
   }
 };

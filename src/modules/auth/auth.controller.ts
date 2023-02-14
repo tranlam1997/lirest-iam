ee929d6429ea9b55028cd25504e2edc91fd341a7
Tran Tuan Lam <foet1997@gmail.com>
2023-02-15 01:19:57 +0700

    add auth dto layer

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,12 +2,12 @@ import { asyncHandler } from '../../shared/helper';
 import { Response, Router } from 'express';
 import { ResultResponse } from '../../shared/response-format';
 import { AuthService } from './auth.service';
-import { LoginRequestBody, RegisterRequestBody } from './interfaces/users.interface';
+import { RegisterRequestDTO, LoginRequestDTO } from './dto/users.dto';
 
 const AuthRouter = Router();
 
 export default (router: Router) => {
-  AuthRouter.post('/register', asyncHandler(async (req: RegisterRequestBody, res: Response) => {
+  AuthRouter.post('/register', asyncHandler(async (req: RegisterRequestDTO, res: Response) => {
     const role = await AuthService.register(req.body);
     return ResultResponse.info(res, {
       response: {
@@ -18,7 +18,7 @@ export default (router: Router) => {
 
   AuthRouter.post(
     '/login',
-    asyncHandler(async (req: LoginRequestBody, res: Response) => {
+    asyncHandler(async (req: LoginRequestDTO, res: Response) => {
       const role = await AuthService.login(req.body);
 
       return ResultResponse.info(res, {

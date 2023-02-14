bc9e60fc72daf7843a68f8bb901a24e6069c8e91
Tran Tuan Lam <foet1997@gmail.com>
2023-02-14 23:53:48 +0700

    remove role module

--- src/modules/roles/interfaces/roles.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Role {
-  name: string;
-  description?: string;
-  permissions?: {
-    subject: string;
-    action: string;
-    conditions?: { [key: string]: any };
-  }[];
-}
--- src/modules/roles/roles.repository.ts
+++
@@ -1,5 +0,0 @@
-import BaseRepository from '../../base/repository';
-import { Role } from './interfaces/roles.interface';
-import RoleModel from './roles.schema';
-
-export const RolesRepository = BaseRepository<Role>(RoleModel)

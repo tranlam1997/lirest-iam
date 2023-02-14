e270ca6f33f6de0f0804b323701791ed11ca5f8c
Tran Tuan Lam <foet1997@gmail.com>
2023-02-14 23:53:14 +0700

    remove casl package and abilites module

--- src/modules/abilities/abilities.service.ts
+++
@@ -1,8 +0,0 @@
-import { PureAbility } from '@casl/ability';
-import { Permission } from "./interfaces/abilities.interface";
-
-export const AbilitesService = {
-  async createAbilities(permissions: Permission[]) {
-    return new PureAbility(permissions);
-  }
-};
--- src/modules/abilities/interfaces/abilities.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface Permission {
-  subject: string;
-  action: string;
-  conditions: { [key: string]: any };
-}

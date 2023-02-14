66d2735a930cbad595d864d73a66a2d45625adf9
Tran Tuan Lam <foet1997@gmail.com>
2023-02-15 01:32:30 +0700

    add dto layer to actions module

--- src/modules/actions/actions.controller.ts
+++ src/modules/actions/actions.controller.ts
@@ -1,14 +1,30 @@
 import { asyncHandler } from '../../shared/helper';
 import { Request, Response, Router } from 'express';
 import { ActionsService } from './actions.service';
+import { CreateActionRequestDTO } from './dto/actions.dto';
 
 const ActionsRouter = Router();
 
 export default (app: Router) => {
-  ActionsRouter.post('/', asyncHandler(async (req: Request, res: Response) => {
-    const role = await ActionsService.createAction(req.body);
-    return res.status(201).json(role);
-  }))
+  ActionsRouter.route('/')
+    /**
+     * Create new action
+     */
+    .post(
+      asyncHandler(async (req: CreateActionRequestDTO, res: Response) => {
+        const role = await ActionsService.createAction(req.body);
+        return res.status(201).json(role);
+      }),
+    )
+    /**
+     * Get all actions
+     */
+    .get(
+      asyncHandler(async (_req: Request, res: Response) => {
+        const actions = await ActionsService.getAllActions();
+        return res.status(200).json(actions);
+      }),
+    );
 
   app.use('/actions', ActionsRouter);
 };
---
+++ src/modules/actions/dto/actions.dto.ts
@@ -0,0 +1,4 @@
+import { BaseBodyRequest } from "@src/base/request";
+import { Action } from "../interfaces/actions.interface";
+
+export type CreateActionRequestDTO = BaseBodyRequest<Action>

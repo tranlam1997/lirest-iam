e0c59599090c21304b2597c02e7ba549fc46974b
Tran Tuan Lam <foet1997@gmail.com>
2023-03-06 22:53:43 +0700

    update docker env

--- scripts/decrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "${scriptDir}/.." || exit 1;
-export SOPS_AGE_KEY_FILE=$(pwd)/age-key.txt
-sops -d -i docker.env
--- scripts/encrypt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "${scriptDir}/.." || exit 1;
-sops -e -i docker.env

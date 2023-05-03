f3a06a894a77b248bfe356f840206e47799d0fd5
Tran Tuan Lam <foet1997@gmail.com>
2023-05-04 03:57:25 +0700

    add cors middleware

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -7,11 +7,15 @@ import ResourcesController from '@src/modules/resources/resources.controller';
 import PermissionsController from '@src/modules/permissions/permissions.controller';
 import PoliciesController from '@src/modules/policies/policies.controller';
 import config from 'config';
+import cors from 'cors';
 
 const router = Router();
 const baseUrl = config.get<string>('service.baseUrl');
 
 export default (app: Application) => {
+  app.use(cors({
+    origin: '*',
+  }))
   ActionsController(router);
   SubjectsController(router);
   AuthController(router);
